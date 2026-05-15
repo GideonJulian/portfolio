6d4ca3322467e9cdca989a9c084cb6e5f6b5a153
GideonJulian <gideonchinonso77@gmail.com>
2026-05-15 13:27:51 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Portfolio from './Portfolio';
+import Portfolio from "./Portfolio";
 // import { Analytics } from "@vercel/analytics/next"
 export default function App() {
   return <Portfolio />;
